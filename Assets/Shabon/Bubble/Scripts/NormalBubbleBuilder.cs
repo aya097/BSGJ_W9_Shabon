0bd54b4eae63d98be29b4faa924f5269485ceb33
MwRano <hitoki0327@icloud.com>
2025-05-15 22:17:13 +0900

    add NormalBubbleBuilder

---
+++ Assets/Shabon/Bubble/Scripts/NormalBubbleBuilder.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Shabon.Bubble
+{
+    /// <summary>
+    /// NormalBubbleの個性ビルドを管理クラス
+    /// </summary>
+    public class NormalBubbleBuilder : IBubbleBuilder
+    {
+        /// <summary>
+        /// 個性を付与するメソッド
+        /// </summary>
+        public void Build(IBubbleMono bubbleMono)
+        {
+            // のちのち実装
+            Debug.Log("個性を付与します");
+            
+            
+        }
+
+
+    }
+}
