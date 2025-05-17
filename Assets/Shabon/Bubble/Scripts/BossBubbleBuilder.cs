0e5d49146b5136ba7783e931645b32c849fd4ea0
MwRano <hitoki0327@icloud.com>
2025-05-17 23:11:22 +0900

    add BossBubbleBuilder

---
+++ Assets/Shabon/Bubble/Scripts/BossBubbleBuilder.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Shabon.Bubble
+{
+    /// <summary>
+    /// BossBubbleの個性を付与するクラス
+    /// </summary>
+    public class BossBubbleBuilder : IBubbleBuilder
+    {
+        /// <summary>
+        /// 個性を付与するメソッド
+        /// </summary>
+        public void Build(IBubbleMono bubbleMono)
+        {
+            // のちのち実装
+            Debug.Log("個性を付与します");
+            
+        }
+
+    }
+}
