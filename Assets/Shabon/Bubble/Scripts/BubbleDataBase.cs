77efd8fbaa411241083b563f28fe18ffbef64e2b
MwRano <hitoki0327@icloud.com>
2025-05-17 23:51:43 +0900

    add BubbleDataBase

---
+++ Assets/Shabon/Bubble/Scripts/BubbleDataBase.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Shabon.Bubble;
+using System.Collections.Generic;
+using System;
+
+[CreateAssetMenu(fileName = "BubbleDataBase", menuName = "Scriptable Objects/BubbleDataBase")]
+public class BubbleDataBase : ScriptableObject
+{
+    [Serializable]
+    public class BubbleData
+    {
+        public BubbleType bubbleType;
+        public GameObject bubblePrefab;
+    }
+
+    public List<BubbleData> bubblePrefabSet;
+}
