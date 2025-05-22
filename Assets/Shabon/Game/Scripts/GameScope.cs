f3cef55c8e516628f2da8ff8d644f05a1653643e
MwRano <noratohira@gmail.com>
2025-05-22 19:26:40 +0900

    fix GameScope

--- Assets/Shabon/Game/Scripts/GameScope.cs
+++ Assets/Shabon/Game/Scripts/GameScope.cs
@@ -32,7 +32,7 @@ namespace Shabon.Game
             builder.RegisterComponentInHierarchy<BreathGetterViewMono>();
             builder.Register<BubbleMono>(Lifetime.Scoped);
             builder.RegisterComponentInHierarchy<WaitingAreaCheckerMono>().As<IAreaChecker>();
-            builder.Register<BubbleChain>(Lifetime.Scoped);
+            builder.Register<BubbleChain>(Lifetime.Scoped).As<IBubbleChain>();
 
             // Param
             builder.RegisterInstance(gameRuleParam).AsImplementedInterfaces();
