8468254417096ecdc29bdde3a21d45069f575bd0
MwRano <noratohira@gmail.com>
2025-05-22 19:24:43 +0900

    add IBubbleChain

---
+++ Assets/Shabon/Bubble/Interfaces/IBubbleChain.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Shabon.Bubble
+{
+    public interface IBubbleChain
+    {
+        void ExecuteBubbleChain(Transform bubbleMonoTransform, float chainRadius);
+    }
+}
+
