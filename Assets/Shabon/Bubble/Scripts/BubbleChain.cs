e9f484e5f5652372bc24da88c3babd061398b7f9
MwRano <noratohira@gmail.com>
2025-05-22 18:14:16 +0900

    add BubbleChain

---
+++ Assets/Shabon/Bubble/Scripts/BubbleChain.cs
@@ -0,0 +1,26 @@
+#nullable enable
+
+using UnityEngine;
+
+namespace Shabon.Bubble
+{
+    /// <summary>
+    /// バブルの連鎖処理を行うクラス
+    /// </summary>
+    public class BubbleChain
+    {
+        /// <summary>
+        /// 指定したバブルを中心に一定範囲内のバブルも連鎖して割る
+        /// </summary>
+        private void ExecuteBubbleChain(Transform bubbleMonoTransform, float chainRadius)
+        {
+            Collider[] aroudBubbleColliders = Physics.OverlapSphere(bubbleMonoTransform.position, chainRadius);
+            foreach (Collider aroudBubbleCollider in aroudBubbleColliders)
+            {
+                IBubbleMono aroundBubbleMono = aroudBubbleCollider.gameObject.GetComponent<IBubbleMono>();
+                aroundBubbleMono.InvokeOnDead();
+            }
+        }
+    }
+}
+
