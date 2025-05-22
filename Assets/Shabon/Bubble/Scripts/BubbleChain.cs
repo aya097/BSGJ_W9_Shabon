3d479d9b132b7d59aa27a4ef15c147dec581a2e3
MwRano <noratohira@gmail.com>
2025-05-22 19:28:30 +0900

    fix BubbleChain

--- Assets/Shabon/Bubble/Scripts/BubbleChain.cs
+++ Assets/Shabon/Bubble/Scripts/BubbleChain.cs
@@ -7,12 +7,12 @@ namespace Shabon.Bubble
     /// <summary>
     /// バブルの連鎖処理を行うクラス
     /// </summary>
-    public class BubbleChain
+    public class BubbleChain : IBubbleChain
     {
         /// <summary>
         /// 指定したバブルを中心に一定範囲内のバブルも連鎖して割る
         /// </summary>
-        private void ExecuteBubbleChain(Transform bubbleMonoTransform, float chainRadius)
+        public void ExecuteBubbleChain(Transform bubbleMonoTransform, float chainRadius)
         {
             Collider[] aroudBubbleColliders = Physics.OverlapSphere(bubbleMonoTransform.position, chainRadius);
             foreach (Collider aroudBubbleCollider in aroudBubbleColliders)
