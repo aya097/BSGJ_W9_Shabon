e93ccb7485ca89b344363b121d92ea987acbd60f
あ <sojiro1018@icloud.com>
2025-05-25 00:41:16 +0900

    Add Clap

--- Assets/Shabon/Clap/ClapViewMono.cs
+++ Assets/Shabon/Clap/ClapViewMono.cs
@@ -1,21 +1,18 @@
+#nullable enable
 using UnityEngine;
-using System.Linq;
 using VContainer;
-using Shabon.Input;
 
 namespace Shabon.Clap
 {
     public class ClapViewMono : MonoBehaviour
     {
         private ClapModel _clapModel = null!;
-        private IInputManager _inputManager = null!;
-        [SerializeField] ClapGetterViewMono clapGetter = null!;
+        private ClapGetterViewMono? clapGetter;
 
         [Inject]
-        public void Initialize(ClapModel clapModel, IInputManager inputManager)
+        public void Initialize(ClapModel clapModel)
         {
             _clapModel = clapModel;
-            _inputManager = inputManager;
         }
 
         void Awake()
@@ -24,7 +21,6 @@ namespace Shabon.Clap
             if (clapGetter == null)
             {
                 clapGetter = GetComponentInChildren<ClapGetterViewMono>();
-
             }
         }
 
@@ -32,29 +28,6 @@ namespace Shabon.Clap
         {
             _clapModel.ExecuteClap(strength);
         }
-
-        void Update()
-        {
-            if (_clapModel == null)
-            {
-                Debug.LogError("ClapModel is null. Ensure it is properly injected.");
-                return;
-            }
-
-            if (clapGetter == null)
-            {
-                Debug.LogError("ClapGetterViewMono is not assigned in the inspector.");
-                return;
-            }
-
-            if (_inputManager.GetClap())
-            {
-                var bubbles = clapGetter.GetBubbleMonos();
-                if (bubbles != null && bubbles.Any())
-                {
-                    _clapModel.ExecuteClap(1f);
-                }
-            }
-        }
     }
 }
+
