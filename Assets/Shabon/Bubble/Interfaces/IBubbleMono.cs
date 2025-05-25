5bcff7b7c192f78892575c84693d047c69109db8
MwRano <hitoki0327@icloud.com>
2025-05-25 19:45:54 +0900

    add bubble score

--- Assets/Shabon/Bubble/Interfaces/IBubbleMono.cs
+++ Assets/Shabon/Bubble/Interfaces/IBubbleMono.cs
@@ -6,6 +6,7 @@ namespace Shabon.Bubble
 {
     public interface IBubbleMono
     {
+        int BubbleScore{ get; }
         Transform Transform { get; }
         void InvokeOnReach();
         void InvokeOnDead();
