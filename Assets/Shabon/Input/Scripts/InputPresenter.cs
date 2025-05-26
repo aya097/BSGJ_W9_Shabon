b001fd01f01e9e90c33cba11f368d8ea01b64b1f
aya097 <akase097@gmail.com>
2025-05-26 16:12:28 +0900

    add speed * deltatime

--- Assets/Shabon/Input/Scripts/InputPresenter.cs
+++ Assets/Shabon/Input/Scripts/InputPresenter.cs
@@ -35,10 +35,11 @@ namespace Shabon.Input
         float _ratio = 0f;
         void ITickable.Tick()
         {
+
             // Breath
             float horizontal = _inputManager.GetHorizontalDirection();
             float amount = _inputManager.GetBreath();
-            _ratio += horizontal * 0.001f;  // 適当な値
+            _ratio += horizontal * 0.5f * Time.deltaTime;  // 適当な値
             _ratio = Mathf.Max(-1, _ratio);
             _ratio = Mathf.Min(1, _ratio);
 
