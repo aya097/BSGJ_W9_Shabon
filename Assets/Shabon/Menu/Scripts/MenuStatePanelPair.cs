47566831dace132eab30a2454a10e332d26e5acf
MwRano <noratohira@gmail.com>
2025-05-30 20:27:22 +0900

    refact MenuViewMono

---
+++ Assets/Shabon/Menu/Scripts/MenuStatePanelPair.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Shabon.Menu
+{
+    [Serializable]
+    public class MenuStatePanelPair
+    {
+        public MenuState menuState;
+        public GameObject panel;
+    }
+}
+
--- Assets/Shabon/Menu/Scripts/MenuViewMono.cs
+++ Assets/Shabon/Menu/Scripts/MenuViewMono.cs
@@ -3,78 +3,138 @@ using UnityEngine.UI;
 using System;
 using VContainer;
 using R3;
+using System.Linq;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
-/// <summary>
-/// メニュー画面のUIを管理するクラス
-/// </summary>
-public class MenuViewMono : MonoBehaviour
+namespace Shabon.Menu
 {
-    // Pause Canvas
-    [SerializeField] private Canvas _pauseCanvas;
-    [SerializeField] private Button _backGameButton;
-    [SerializeField] private Button _controlsButton;
-    [SerializeField] private Button _optionButton;
-    [SerializeField] private Button _backTitleButton;
-
-    // Option Canvas
-    [SerializeField] private Canvas _optionCanvas;
-    [SerializeField] private Button _volumeButton;
-    [SerializeField] private Button _resolutionButton;
-
-    // Volume Canvas
-    [SerializeField] private Canvas _volumeCanvas;
-
-    // Resolution Canvas
-    [SerializeField] private Canvas _resolutionCanvas;
-
-
-    public event Action OnBackGameButtonClicked;
-    public event Action OnControlsButtonClicked;
-    public event Action OnOptionButtonClicked;
-    public event Action OnBackTitleButtonClicked;
-
-    public event Action OnVolumeButtonClicked;
-    public event Action OnResolutionButtonClicked;
-
-    [Inject]
-    public void Initialize()
+    public enum ResolutionState
     {
-        // ボタンのクリックイベントを登録
-        _backGameButton.OnClickAsObservable()
-        .Subscribe(_ => OnBackGameButtonClicked.Invoke())
-        .AddTo(this);
-
-        _controlsButton.OnClickAsObservable()
-        .Subscribe(_ => OnControlsButtonClicked.Invoke())
-        .AddTo(this);
-
-        _optionButton.OnClickAsObservable()
-        .Subscribe(_ => OnOptionButtonClicked.Invoke())
-        .AddTo(this);
-
-        _backTitleButton.OnClickAsObservable()
-        .Subscribe(_ => OnBackTitleButtonClicked.Invoke())
-        .AddTo(this);
-
-        _volumeButton.OnClickAsObservable()
-        .Subscribe(_ => OnVolumeButtonClicked.Invoke())
-        .AddTo(this);
-
-        _resolutionButton.OnClickAsObservable()
-        .Subscribe(_ => OnResolutionButtonClicked.Invoke())
-        .AddTo(this);
-
+        High,
+        Middle,
+        Low
     }
 
-    // 指定したcanvasを表示するメソッド
-    public void ShowCanvas(Canvas canvasToShow)
+    /// <summary>
+    /// メニュー画面のUIを管理するクラス
+    /// </summary>
+    public class MenuViewMono : MonoBehaviour
     {
-        // 全てのキャンバスを非表示にする
-        _pauseCanvas.gameObject.SetActive(false);
-        _volumeCanvas.gameObject.SetActive(false);
-        _resolutionCanvas.gameObject.SetActive(false);
+        [SerializeField] private List<MenuStatePanelPair> _menuStatePanelPairs;
 
-        // 指定したキャンバスを表示する
-        canvasToShow.gameObject.SetActive(true);
+        // Pause Canvas
+        [Header("ポーズ画面")]
+        [SerializeField] private Button _backGameButton;
+        [SerializeField] private Button _controlsButton;
+        [SerializeField] private Button _optionButton;
+        [SerializeField] private Button _backTitleButton;
+
+
+        // Option Canvas
+        [Header("オプション画面")]
+        [SerializeField] private Button _volumeButton;
+        [SerializeField] private Button _resolutionButton;
+
+
+        // Resolution Canvas
+        [Header("解像度設定画面")]
+        [SerializeField] private Button _highResolutionButton;
+        [SerializeField] private Button _middleResolutionButton;
+        [SerializeField] private Button _lowResolutionButton;
+
+
+        public event Action OnBackGameButtonClicked;
+        public event Action OnBackTitleButtonClicked;
+        public event Action<MenuState> OnPanelSwitchButtonClicked;
+        public event Action<ResolutionState> OnChangeResolutionButtonClicked;
+
+
+        [Inject]
+        public void Initialize()
+        {
+            // ポーズ画面のUIのボタン
+            _backGameButton.OnClickAsObservable()
+                .Subscribe(_ => OnBackGameButtonClicked.Invoke())
+                .AddTo(this);
+
+            _backTitleButton.OnClickAsObservable()
+                .Subscribe(_ => OnBackTitleButtonClicked.Invoke())
+                .AddTo(this);
+
+
+            // panel切り替えのボタン
+            _controlsButton.OnClickAsObservable()
+                .Subscribe(_ => OnPanelSwitchButtonClicked.Invoke(MenuState.Controls))
+                .AddTo(this);
+
+            _optionButton.OnClickAsObservable()
+                .Subscribe(_ => OnPanelSwitchButtonClicked.Invoke(MenuState.Option))
+                .AddTo(this);
+
+            _volumeButton.OnClickAsObservable()
+                .Subscribe(_ => OnPanelSwitchButtonClicked.Invoke(MenuState.Volume))
+                .AddTo(this);
+
+            _resolutionButton.OnClickAsObservable()
+                .Subscribe(_ => OnPanelSwitchButtonClicked.Invoke(MenuState.Resolution))
+                .AddTo(this);
+
+            // 解像度画面のボタン
+            _highResolutionButton.OnClickAsObservable()
+                .Subscribe(_ => OnChangeResolutionButtonClicked.Invoke(ResolutionState.High))
+                .AddTo(this);
+
+            _middleResolutionButton.OnClickAsObservable()
+                .Subscribe(_ => OnChangeResolutionButtonClicked.Invoke(ResolutionState.Middle))
+                .AddTo(this);
+
+            _lowResolutionButton.OnClickAsObservable()
+                .Subscribe(_ => OnChangeResolutionButtonClicked.Invoke(ResolutionState.Low))
+                .AddTo(this);
+
+        }
+
+        // 指定したmenuStateに対応するpanelを表示するメソッド
+        public void ShowPanel(MenuState menuState)
+        {
+            foreach (MenuStatePanelPair menuStatePanelPair in _menuStatePanelPairs)
+            {
+                // panelを非表示
+                menuStatePanelPair.panel.SetActive(false);
+
+                // 指定されたpanelの設定
+                if (menuStatePanelPair.menuState == menuState)
+                {
+                    // panelの表示
+                    menuStatePanelPair.panel.SetActive(true);
+
+                    // panel上で初期選択されるUIを設定
+                    Selectable[] childObjects = menuStatePanelPair.panel.GetComponentsInChildren<Selectable>(true);
+                    Selectable selectableObject = childObjects.FirstOrDefault(b => b.gameObject.activeInHierarchy && b.interactable);
+                    if (selectableObject != null)
+                        EventSystem.current.SetSelectedGameObject(selectableObject.gameObject);
+                }
+            }
+
+        }
+        
+        // 指定した解像度に更新するメソッド
+        public void UpdateResolutionDisplay(ResolutionState resolutionState)
+        {
+            switch (resolutionState)
+            {
+                case ResolutionState.High:
+                    Debug.Log("解像度を 高 にしました");
+                    break;
+                case ResolutionState.Middle:
+                    Debug.Log("解像度を 中 にしました");
+                    break;
+                case ResolutionState.Low:
+                    Debug.Log("解像度を 低 にしました");
+                    break;
+            }
+        }
     }
 }
+
