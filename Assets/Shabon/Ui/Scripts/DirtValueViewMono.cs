4d17f20208000d68676e8ee34dde8786c560e328
aya097 <akase097@gmail.com>
2025-06-07 11:53:35 +0900

    add DirtValueViewMono

---
+++ Assets/Shabon/Ui/Scripts/DirtValueViewMono.cs
@@ -0,0 +1,33 @@
+#nullable enable
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shabon.Ui
+{
+    /// <summary>
+    /// 汚れゲージのViewClassです。
+    /// </summary>
+    public class DirtValueViewMono : MonoBehaviour
+    {
+        [SerializeField] Image fillImage = null!;   // ゲージの画像
+        private float _value; // 現在表示してる値
+
+        /// <summary>
+        /// 値を反映します
+        /// </summary>
+        /// <param name="currentValue">現在の値</param>
+        /// <param name="minValue">最小値</param>
+        /// <param name="maxValue">最大値</param>
+        public void SetValue(float currentValue, float minValue, float maxValue)
+        {
+            float range = maxValue - minValue;
+            if (range <= 0)
+            {
+                Debug.LogWarning($"汚れ値の幅が0以下です。${range}");
+            }
+
+            _value = currentValue / range;
+            fillImage.fillAmount = _value;
+        }
+    }
+}
