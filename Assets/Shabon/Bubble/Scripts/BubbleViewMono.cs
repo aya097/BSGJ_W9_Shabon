e29aa06f50d5c42aee4a4b9d90541bbc48e3ee3b
MwRano <noratohira@gmail.com>
2025-06-07 21:42:58 +0900

    fix animator controller type

---
+++ Assets/Shabon/Bubble/Scripts/BubbleViewMono.cs
@@ -0,0 +1,33 @@
+#nullable enable
+using UnityEngine;
+
+/// <summary>
+/// Bubbleの見た目を管理するクラス
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class BubbleViewMono : MonoBehaviour
+{
+    private Animator _bubbleAnimator;
+
+    public void Awake()
+    {
+        _bubbleAnimator = GetComponent<Animator>();
+    }
+
+    public void SetAnimatorController(RuntimeAnimatorController bubbleAnimatorController)
+    {
+        _bubbleAnimator.runtimeAnimatorController = bubbleAnimatorController;
+    }
+
+    // 割られた時のアニメーションを再生するメソッド
+    public void PlayClappedAnimation()
+    {
+        _bubbleAnimator.SetTrigger("ClapTrigger");
+    }
+
+    // 息が吹かれた時のアニメーションを再生するメソッド
+    public void PlayBreathedAnimation()
+    {
+        Debug.Log("息が吹かれたときのアニメーションを再生");
+    }
+}
