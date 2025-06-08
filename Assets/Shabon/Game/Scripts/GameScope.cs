4df1ae4f5db453e2eadf4023701008d096481028
aya097 <akase097@gmail.com>
2025-06-08 16:20:54 +0900

    dix error

--- Assets/Shabon/Game/Scripts/GameScope.cs
+++ Assets/Shabon/Game/Scripts/GameScope.cs
@@ -79,9 +79,6 @@ namespace Shabon.Game
             builder.RegisterComponentInHierarchy<DirtValueViewMono>();
             builder.RegisterEntryPoint<InGameUiPresenter>();
             builder.Register<ComboSpawner>(Lifetime.Scoped);
-
-            // ResultButton を登録
-            builder.RegisterComponentInHierarchy<ResultButton>();
         }
     }
 }
