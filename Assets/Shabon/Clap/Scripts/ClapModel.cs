abdd0c1a19db0b2f33ca27fdafe38d240ba650ec
あ <sojiro1018@icloud.com>
2025-06-12 15:35:41 +0900

    Add ClapCoolTime

--- Assets/Shabon/Clap/Scripts/ClapModel.cs
+++ Assets/Shabon/Clap/Scripts/ClapModel.cs
@@ -1,13 +1,18 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using Shabon.Bubble;
 using VContainer;
+using R3;
 
 namespace Shabon.Clap
 {
     public class ClapModel
     {
         private readonly IBubbleHandler _bubbleHandler;
+        private bool _canClap = true; // Clap可能かどうかを管理するフラグ
+        private const float ClapCoolTime = 5f; // クールダウン時間
+        private readonly CompositeDisposable _disposable = new();
+
         [Inject]
         public ClapModel(IBubbleHandler bubbleHandler)
         {
@@ -16,7 +21,27 @@ namespace Shabon.Clap
 
         public void ApplyClap(float strength)
         {
-            _bubbleHandler.ApplyClap(strength);
+            if (_canClap)
+            {
+                _bubbleHandler.ApplyClap(strength);
+                _canClap = false; // Clapを使用不可に設定
+
+                // Observable.Timerを使用してクールタイムを管理
+                Observable.Timer(TimeSpan.FromSeconds(ClapCoolTime))
+                    .Subscribe(_ => ResetClap())
+                    .AddTo(_disposable);
+            }
+
+        }
+
+        private void ResetClap()
+        {
+            _canClap = true;
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
         }
     }
 }
