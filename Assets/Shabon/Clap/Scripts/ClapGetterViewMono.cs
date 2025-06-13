543668525b868893b29c003b3d63e35d67eb6b9c
あ <sojiro1018@icloud.com>
2025-06-13 11:08:43 +0900

    Add Score

--- Assets/Shabon/Clap/Scripts/ClapGetterViewMono.cs
+++ Assets/Shabon/Clap/Scripts/ClapGetterViewMono.cs
@@ -8,6 +8,8 @@ namespace Shabon.Clap
 {
     public class ClapGetterViewMono : MonoBehaviour
     {
+        [SerializeField] private float radius = 1.0f; // 円の半径を指定
+
         // BubbleMonoを取得する
         public IEnumerable<IBubbleMono> GetBubbleMonos()
         {
@@ -18,13 +20,8 @@ namespace Shabon.Clap
                 return Enumerable.Empty<IBubbleMono>();
             }
 
-            Vector3 boxSize = new Vector3(
-                transform.localScale.x * boxCol.size.x,
-                transform.localScale.y * boxCol.size.y,
-                transform.localScale.z * boxCol.size.z
-            ); // Colliderのサイズ取得
-
-            Collider[] hits = Physics.OverlapBox(transform.position, boxSize * 0.5f, transform.rotation);
+            // 円形の範囲でColliderを取得
+            Collider[] hits = Physics.OverlapSphere(transform.position, radius);
 
             // IBubbleMonoを取得し、nullでないものだけを集める
             return hits
@@ -36,6 +33,12 @@ namespace Shabon.Clap
                 })
                 .Where(x => x != null)
                 .Select(x => x!);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.5f); // 半透明の赤色
+            Gizmos.DrawSphere(transform.position, radius); // 範囲を描画
         }
     }
 }
