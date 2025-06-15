f2eeed2bc3df2aab77afec3565f3f9d300968f8f
aya097 <akase097@gmail.com>
2025-06-15 16:48:25 +0900

    add waiting area view

--- Assets/Shabon/Bubble/Scripts/WaitingAreaCheckerMono.cs
+++ Assets/Shabon/Bubble/Scripts/WaitingAreaCheckerMono.cs
@@ -9,18 +9,19 @@ namespace Shabon.Bubble
     /// </summary>
     public class WaitingAreaCheckerMono : MonoBehaviour, IAreaChecker
     {
-        [SerializeField] BoxCollider judgeArea = null!;
+        [SerializeField] SphereCollider judgeArea = null!;
 
         public bool IsInArea(Vector3 position)
         {
             // エリア内の中心とpositionの差
             Vector3 diff = position - judgeArea.transform.position;
             // 差がsizeの半分以下ならbox内
-            bool inX = Mathf.Abs(diff.x) <= judgeArea.size.x / 2;
-            bool inY = Mathf.Abs(diff.y) <= judgeArea.size.y / 2;
-            bool inZ = Mathf.Abs(diff.z) <= judgeArea.size.z / 2;
+            // bool inX = Mathf.Abs(diff.x) <= judgeArea.size.x / 2;
+            // bool inY = Mathf.Abs(diff.y) <= judgeArea.size.y / 2;
+            // bool inZ = Mathf.Abs(diff.z) <= judgeArea.size.z / 2;
 
-            return inX && inY && inZ;
+            // 差が半径以下なら中
+            return diff.sqrMagnitude <= Mathf.Pow(judgeArea.radius, 2);
         }
     }
 
