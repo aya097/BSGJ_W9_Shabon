7bc7c5d4da895a147d213eb2f13ef5de000f2fce
aya097 <akase097@gmail.com>
2025-06-16 14:14:46 +0900

    add ring

--- Assets/Shabon/Clap/Scripts/ClapViewMono.cs
+++ Assets/Shabon/Clap/Scripts/ClapViewMono.cs
@@ -13,11 +13,17 @@ namespace Shabon.Clap
     /// </summary>
     public class ClapViewMono : MonoBehaviour
     {
+        [Header("床のうねりについて")]
         [SerializeField] private Material waveMaterial = null!;
         [SerializeField] private GameObject wavedObject = null!;
         [SerializeField] private float waveRadius;
-        [SerializeField] private float duration;
+        [SerializeField] private float waveDuration;
 
+        [Header("輪っかについて")]
+        [SerializeField] private GameObject ringObject = null!;
+        [SerializeField] private Ease ringEase;
+        [SerializeField] private float ringRadius;
+        [SerializeField] private float ringDuration;
 
         [Inject]
         public void Initialize(ClapModel clapModel)
@@ -37,16 +43,34 @@ namespace Shabon.Clap
         {
             float radiusRatio = waveRadius / wavedObject.transform.localScale.x;
             // 0から半径（全体の大きさに対する割合）まで変化
-            LMotion.Create(0f, radiusRatio, duration)
+            LMotion.Create(0f, radiusRatio, waveDuration)
                 .WithEase(Ease.Linear)
-                .WithOnComplete(() => SetRadius(0f))
-                .Bind(value => SetRadius(value))
+                .WithOnComplete(() => SetWave(0f))
+                .Bind(value => SetWave(value))
+                .AddTo(this);
+
+            // リングの半径
+            LMotion.Create(0f, ringRadius, ringDuration)
+                .WithEase(ringEase)
+                .WithOnComplete(() => SetRing(0f))
+                .Bind(value => SetRing(value))
+                .AddTo(this);
+            // 色変化
+            var ringRenderer = ringObject.GetComponent<SpriteRenderer>();
+            LMotion.Create(1f, 0f, ringDuration)
+                .WithEase(Ease.OutQuart)
+                .BindToColorA(ringRenderer)
                 .AddTo(this);
         }
 
-        private void SetRadius(float radius)
+        private void SetWave(float radius)
         {
             waveMaterial.SetFloat("_Radius", radius);
+        }
+
+        private void SetRing(float radius)
+        {
+            ringObject.transform.localScale = Vector3.one * radius;
         }
 
     }
