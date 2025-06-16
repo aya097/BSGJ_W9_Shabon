e344a82e25de4e8fab08399b8b210cf233ae6f59
aya097 <akase097@gmail.com>
2025-06-16 14:19:08 +0900

    add toggle enable effect

--- Assets/Shabon/Clap/Scripts/ClapViewMono.cs
+++ Assets/Shabon/Clap/Scripts/ClapViewMono.cs
@@ -14,12 +14,15 @@ namespace Shabon.Clap
     public class ClapViewMono : MonoBehaviour
     {
         [Header("床のうねりについて")]
+        [SerializeField] private bool waveEnable = true;
         [SerializeField] private Material waveMaterial = null!;
         [SerializeField] private GameObject wavedObject = null!;
         [SerializeField] private float waveRadius;
         [SerializeField] private float waveDuration;
 
         [Header("輪っかについて")]
+        [SerializeField] private bool ringEnable = true;
+
         [SerializeField] private GameObject ringObject = null!;
         [SerializeField] private Ease ringEase;
         [SerializeField] private float ringRadius;
@@ -41,26 +44,32 @@ namespace Shabon.Clap
 
         private void Play()
         {
-            float radiusRatio = waveRadius / wavedObject.transform.localScale.x;
-            // 0から半径（全体の大きさに対する割合）まで変化
-            LMotion.Create(0f, radiusRatio, waveDuration)
-                .WithEase(Ease.Linear)
-                .WithOnComplete(() => SetWave(0f))
-                .Bind(value => SetWave(value))
-                .AddTo(this);
+            if (waveEnable)
+            {
+                float radiusRatio = waveRadius / wavedObject.transform.localScale.x;
+                // 0から半径（全体の大きさに対する割合）まで変化
+                LMotion.Create(0f, radiusRatio, waveDuration)
+                    .WithEase(Ease.Linear)
+                    .WithOnComplete(() => SetWave(0f))
+                    .Bind(value => SetWave(value))
+                    .AddTo(this);
+            }
 
-            // リングの半径
-            LMotion.Create(0f, ringRadius, ringDuration)
+            if (ringEnable)
+            {
+                // リングの半径
+                LMotion.Create(0f, ringRadius, ringDuration)
                 .WithEase(ringEase)
                 .WithOnComplete(() => SetRing(0f))
                 .Bind(value => SetRing(value))
                 .AddTo(this);
-            // 色変化
-            var ringRenderer = ringObject.GetComponent<SpriteRenderer>();
-            LMotion.Create(1f, 0f, ringDuration)
-                .WithEase(Ease.OutQuart)
-                .BindToColorA(ringRenderer)
-                .AddTo(this);
+                // 色変化
+                var ringRenderer = ringObject.GetComponent<SpriteRenderer>();
+                LMotion.Create(1f, 0f, ringDuration)
+                    .WithEase(Ease.OutQuart)
+                    .BindToColorA(ringRenderer)
+                    .AddTo(this);
+            }
         }
 
         private void SetWave(float radius)
