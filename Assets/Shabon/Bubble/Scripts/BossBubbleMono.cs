d3783a8de8f2163200602c0dfab953936e3dac61
MwRano <noratohira@gmail.com>
2025-06-30 00:07:36 +0900

    Add Param and Spawn process to BossBubbleMono

--- Assets/Shabon/Bubble/Scripts/BossBubbleMono.cs
+++ Assets/Shabon/Bubble/Scripts/BossBubbleMono.cs
@@ -2,58 +2,60 @@
 
 using System;
 using UnityEngine;
-using VContainer;
-using Shabon.Score;
-using UnityEngine.Video;
-using Unity.VisualScripting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Shabon.Bubble
 {
-    public class BossBubbleMono : BubbleMono
+    public class BossBubbleMono : BubbleMono, IBubbleMono
     {
+        public event Action<IEnumerable<BubbleType>>? OnSpawn;
         public bool IsBreathing
         {
             get { return _isBreathing; }
             set { _isBreathing = value; }
         }
-        public int BossHp
-        {
-            get { return _bossHp; }
-            set { _bossHp = value; }
-        }
-        public bool IsBack
-        {
-            get { return _isBack; }
-            set { _isBack = value; }
-        }
 
+        public int BossHitPoint => _bossHitPoint;
         private bool _isBreathing = false;
         private bool _isBack = false;
-        private int _bossHp = 0;
         private Vector3 _basePosition;
+        private int _bossHitPoint;
+        private float _bubbleSpawnInterval;
+        private int _bubbleSpawnNum;
+        private float _currentTime = 0;
 
-
-        void Start()
+        void Awake()
         {
             _basePosition = gameObject.transform.position;
         }
 
         protected override void Update()
         {
+            // 到達したら動かない
+            if (_isReached) return;
 
             // 停止中だったら動かない
             if (_isStop) return;
 
+            // 後退
             if (_isBack)
             {
-                if (_bubbleMover is BossBubbleMover bossBubbleMover)
-                    bossBubbleMover.MoveBackward(_basePosition);
-                int distance = (int)Vector3.Distance(transform.position, _basePosition);
-                if (distance == 0) _isBack = false;
+                _bubbleMover.MoveBackward(_basePosition);
+                if (Vector3.Distance(transform.position, _basePosition) < 0.001f)
+                    _isBack = false;
+                return;
             }
-            else
+
+            // 前進
+            _bubbleMover.MoveForward();
+
+            // スポーンの処理
+            _currentTime += Time.deltaTime;
+            if (_currentTime >= _bubbleSpawnInterval)
             {
-                _bubbleMover.MoveForward();
+                InvokeOnSpawn();
+                _currentTime = 0;
             }
 
             // waitArea
@@ -64,16 +66,50 @@ namespace Shabon.Bubble
             }
         }
 
-        public override void SetBuildParam(IBubbleMover bubbleMover, BubbleDeath bubbleDeath, IAreaChecker areaChecker, IBubbleData bubbleData)
+        public override void SetBuildParam(
+            IBubbleMover bubbleMover,
+            BubbleDeath bubbleDeath,
+            IAreaChecker areaChecker,
+            IBubbleData bubbleData)
         {
             base.SetBuildParam(bubbleMover, bubbleDeath, areaChecker, bubbleData);
 
-            if (bubbleData is BossBubbleData bossBubbleData)
-            {
-                _bossHp = bossBubbleData.Hp;
-            }
+            _bossHitPoint = bubbleData.BossHitPoint;
+            _bubbleSpawnInterval = bubbleData.BubbleSpawnInterval;
+            _bubbleSpawnNum = bubbleData.BubbleSpawnNum;
         }
-    
+
+        public void Back()
+        {
+            _isBack = true;
+        }
+
+        // HPを減らすメソッド
+        public void DecreaseHp(int damage)
+        {
+            _bossHitPoint -= damage;
+        }
+
+        public void InvokeOnSpawn()
+        {
+            // ランダムなバブルタイプ取得(Noneは除外)
+            List<BubbleType> bubbleTypeList = new();
+            BubbleType randomBubbleType;
+            foreach (var i in Enumerable.Range(0, _bubbleSpawnNum))
+            {
+                do
+                {
+                    Array bubbleTypes = Enum.GetValues(typeof(BubbleType));
+                    int randomIndex = UnityEngine.Random.Range(0, bubbleTypes.Length - 1);
+                    randomBubbleType = (BubbleType)bubbleTypes.GetValue(randomIndex);
+
+                } while (randomBubbleType == BubbleType.None);
+                bubbleTypeList.Add(randomBubbleType);
+            }
+            
+            OnSpawn?.Invoke(bubbleTypeList);
+        }
+        
     }
 }
 
