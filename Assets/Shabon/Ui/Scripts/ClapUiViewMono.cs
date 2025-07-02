b7e6becfe78b6e2ff589c407f48fe61d8c027cd7
aya097 <akase097@gmail.com>
2025-07-02 09:39:44 +0900

    add switch clap ui animation

--- Assets/Shabon/Ui/Scripts/ClapUiViewMono.cs
+++ Assets/Shabon/Ui/Scripts/ClapUiViewMono.cs
@@ -10,10 +10,14 @@ namespace Shabon.Ui
     public class ClapUiViewMono : MonoBehaviour
     {
         [SerializeField] private Image _coolTime = null!;
+        [SerializeField] private GameObject _backLight = null!;
+        [SerializeField] private GameObject _frontLight = null!;
 
         void Awake()
         {
             _coolTime.fillAmount = 1f;
+            _backLight.SetActive(true);
+            _frontLight.SetActive(true);
         }
 
         public void SetCoolTime(float min, float max, float num)
@@ -22,6 +26,19 @@ namespace Shabon.Ui
             if (range <= 0) return;
 
             _coolTime.fillAmount = num / range;
+
+            // クールタイムが終わったらAnimationを再生する
+            if (num >= max)
+            {
+                _backLight.SetActive(true);
+                _frontLight.SetActive(true);
+            }
+            // クールタイム中はAnimationを止める
+            else
+            {
+                _backLight.SetActive(false);
+                _frontLight.SetActive(false);
+            }
         }
     }
 }
