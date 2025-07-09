36f1065bf749ec30f1b37feeed0afe0026261e81
aya097 <akase097@gmail.com>
2025-07-09 18:35:54 +0900

    add tutorial bubble spawner

---
+++ Assets/Shabon/Tutorial/Scripts/TutorialBubbleSpawner.cs
@@ -0,0 +1,104 @@
+#nullable enable
+
+using System.Linq;
+using R3;
+using Shabon.Bubble;
+using Shabon.Param;
+using UnityEngine;
+using VContainer;
+
+namespace Shabon.Tutorial
+{
+    /// <summary>
+    /// TutorialのためのBubbleをスポーンさせる
+    /// </summary>
+    public class TutorialBubbleSpawner
+    {
+        private readonly IPlayerTransform _playerTransform;
+
+        private readonly IBubbleParam _bubbleParam;
+        private readonly BubbleCluster _bubbleCluster;
+        private readonly IBubbleSpawnedArea _bubbleSpawnedArea;
+
+        [Inject]
+        public TutorialBubbleSpawner(
+            IPlayerTransform playerTransform,
+            IBubbleParam bubbleParam,
+            BubbleCluster bubbleCluster,
+            IBubbleSpawnedArea bubbleSpawnedArea)
+        {
+            _playerTransform = playerTransform;
+            _bubbleParam = bubbleParam;
+            _bubbleCluster = bubbleCluster;
+            _bubbleSpawnedArea = bubbleSpawnedArea;
+        }
+
+        // スポーン
+        public void Spawn(BubbleType bubbleType, BubbleSpawnedAreaType spawnArea)
+        {
+            // bubbleType別にdataを取得する
+            IBubbleData bubbleData = _bubbleParam.GetBubbleDataList().Where(b => b.BubbleType == bubbleType).FirstOrDefault();
+
+
+            // スポーン位置を作成
+            Vector3 spawningPosition = DecideSpawningPosition(_bubbleSpawnedArea.GetArea(spawnArea));
+
+            // バブルを生成
+            var bubbleMono = GameObject.Instantiate(bubbleData.BubblePrefab, spawningPosition, Quaternion.identity);
+
+            // Viewを取得
+            var bubbleViewSelector = bubbleMono.gameObject.GetComponentInChildren<BubbleViewSelectorMono>();
+            var bubbleViewMono = bubbleViewSelector.GetViewMono(bubbleType);
+            // 選択されたViewのみActiveにする
+            bubbleViewMono.gameObject.SetActive(true);
+
+            // Clusterに追加
+            _bubbleCluster.Add(bubbleMono);
+        }
+
+        // ビルド
+        public void Build(
+            IBubbleBuildSetter bubbleSetter,
+            IBubbleMono bubbleMono,
+            IBubbleData bubbleData,
+            BubbleViewMono bubbleViewMono)
+        {
+            // BubbleMoverの生成
+            IBubbleMover bubbleMover = new NormalBubbleMover(bubbleMono.Transform, bubbleData.ForwardVelocity, _playerTransform.PlayerTransform);
+
+
+            // プレゼンター処理？
+            Observable.EveryValueChanged(bubbleMono, b => b.IsClapable)
+                .Subscribe(clapable =>
+                {
+                    if (clapable)
+                    {
+                        bubbleViewMono.SetHighlight(HighLightType.Clapable);
+                    }
+                    else
+                    {
+                        bubbleViewMono.SetHighlight(HighLightType.None);
+                    }
+                }).AddTo(bubbleViewMono);
+        }
+
+        private Vector3 DecideSpawningPosition(BoxArea boxArea)
+        {
+            Vector3 rand = new Vector3(Random.value, Random.value, Random.value);
+            // randの範囲を-0.5~0.5に
+            rand = rand - Vector3.one * 0.5f;
+
+            // 座標 + 範囲 * (-1~1)
+            return boxArea.Position + Vector3.Scale(boxArea.Size, rand);
+        }
+
+        private void DestroyBubble(IBubbleMono bubbleMono)
+        {
+            // Clusterから削除
+            _bubbleCluster.Remove(bubbleMono);
+
+            // Destroy
+            GameObject.Destroy(bubbleMono.Transform?.gameObject);
+        }
+    }
+}
