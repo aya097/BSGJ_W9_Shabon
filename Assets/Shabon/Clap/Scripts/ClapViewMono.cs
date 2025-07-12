236cfb7bb4d02a8ec517a8451386837779fef3c6
aya097 <akase097@gmail.com>
2025-07-12 12:24:43 +0900

    add shader in clap view mono

--- Assets/Shabon/Clap/Scripts/ClapViewMono.cs
+++ Assets/Shabon/Clap/Scripts/ClapViewMono.cs
@@ -13,12 +13,11 @@ namespace Shabon.Clap
     /// </summary>
     public class ClapViewMono : MonoBehaviour
     {
-        [Header("床のうねりについて")]
+        [Header("画面のエフェクトについて")]
         [SerializeField] private bool waveEnable = true;
-        [SerializeField] private Material waveMaterial = null!;
-        [SerializeField] private GameObject wavedObject = null!;
-        [SerializeField] private float waveRadius;
-        [SerializeField] private float waveDuration;
+        [SerializeField] private SpriteRenderer waveEffect = null!;
+        [SerializeField] private float maxRadius = 1f;
+        [SerializeField] private float waveSpeed = 0.5f;
 
         [Header("輪っかについて")]
         [SerializeField] private bool ringEnable = true;
@@ -46,12 +45,11 @@ namespace Shabon.Clap
         {
             if (waveEnable)
             {
-                float radiusRatio = waveRadius / wavedObject.transform.localScale.x;
                 // 0から半径（全体の大きさに対する割合）まで変化
-                LMotion.Create(0f, radiusRatio, waveDuration)
+                LMotion.Create(0f, 1f, waveSpeed)
                     .WithEase(Ease.Linear)
                     .WithOnComplete(() => SetWave(0f))
-                    .Bind(value => SetWave(value))
+                    .Bind(value => SetWave(value * maxRadius))
                     .AddTo(this);
             }
 
@@ -74,7 +72,15 @@ namespace Shabon.Clap
 
         private void SetWave(float radius)
         {
-            waveMaterial.SetFloat("_Radius", radius);
+            if (radius == 0)
+            {
+                waveEffect.gameObject.SetActive(false);
+            }
+            else
+            {
+                waveEffect.gameObject.SetActive(true);
+            }
+            waveEffect.material.SetFloat("_Radius", radius);
         }
 
         private void SetRing(float radius)
