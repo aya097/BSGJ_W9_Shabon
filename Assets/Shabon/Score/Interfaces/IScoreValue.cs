0fa9dc68ef462e33c3a31dce871f8e57c9684c50
aya097 <akase097@gmail.com>
2025-07-14 23:49:18 +0900

    add is increase param

--- Assets/Shabon/Score/Interfaces/IScoreValue.cs
+++ Assets/Shabon/Score/Interfaces/IScoreValue.cs
@@ -3,6 +3,7 @@ namespace Shabon.Score
     public interface IScoreValue
     {
         int ScoreNum { get; }
+        bool IsIncrease { get; }
         void Increase(int value);
         void Decrease(int value);
         void Set(int value);
