110e51551fda934e7d7263890d89753d3c51e671
aya097 <akase097@gmail.com>
2025-07-18 00:16:33 +0900

    add PhaseViewMono

---
+++ Assets/Shabon/Ui/Scripts/PhaseViewMono.cs
@@ -0,0 +1,39 @@
+#nullable enable
+using System;
+using LitMotion;
+using LitMotion.Extensions;
+using R3;
+using TMPro;
+using UnityEngine;
+
+namespace Shabon.Ui
+{
+    /// <summary>
+    /// Phaseの切り替わりを表現する
+    /// </summary>
+    public class PhaseViewMono : MonoBehaviour
+    {
+        [SerializeField] private GameObject view = null!;
+        [SerializeField] private TMP_Text phaseText = null!;
+        [SerializeField] private float duration = 1;
+        [SerializeField] private Ease ease;
+
+        public void SetPhase(int phaseNum)
+        {
+            phaseText.text = "Phase " + phaseNum.ToString();
+            LMotion.Create(1500f, 0f, duration)
+                    .WithEase(ease)
+                    .BindToAnchoredPosition3DY(view.GetComponent<RectTransform>());
+
+            Observable.Timer(TimeSpan.FromSeconds(2))
+                .Subscribe(_ =>
+                {
+                    LMotion.Create(0f, 1500f, duration)
+                        .WithEase(ease)
+                        .BindToAnchoredPosition3DY(view.GetComponent<RectTransform>());
+                });
+
+        }
+
+    }
+}
