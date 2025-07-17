0ce4ca7da98c38394045a9ec582971681455e0d2
MwRano <hitoki0327@icloud.com>
2025-07-17 17:13:36 +0900

    feat : Add prologue skip function

--- Assets/Shabon/Title/Scripts/TitlePresenter.cs
+++ Assets/Shabon/Title/Scripts/TitlePresenter.cs
@@ -8,6 +8,7 @@ using Shabon.Input;
 using R3;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Shabon.Title
 {
@@ -16,10 +17,21 @@ namespace Shabon.Title
     /// </summary>
     public class TitlePresenter : IInitializable, IDisposable
     {
+        // skipコマンド用
+        private enum Command
+        {
+            Clap,
+            Breath
+        }
+
         private List<IDisposable> _disposables = new();
 
         // ブレスした時間
         private float _breathContinuousTime = 0f;
+        private List<Command> _skipInputCommand = new List<Command>();
+        private readonly List<Command> _skipCorrectCommand = new List<Command>() { Command.Clap, Command.Breath, Command.Clap}; //skipのコマンド
+        private bool _isStartBreath = false;
+
         [Inject]
         public TitlePresenter(
             TitleViewMono titleViewMono,
@@ -59,8 +71,6 @@ namespace Shabon.Title
             titleViewMono.ProloguePlayableDirector.stopped +=
                 // director => SceneTransition.Transition(SceneName.GameScene);
                 director =>  asyncSceneLoaderMono.CanTransitionGameScene = true;
-;
-                
 
 
             // 入力取得
@@ -70,42 +80,69 @@ namespace Shabon.Title
                     bool isClap = inputManager.GetClap();
                     float horizontal = inputManager.GetHorizontalDirection();
                     float breath = inputManager.GetBreath();
-                    // Start
-                    if (titleModel.CurrentState == TitleState.Start)
+
+                    switch (titleModel.CurrentState)
                     {
-                        // Clapされればゲーム開始
-                        if (isClap)
-                        {
-                            titleModel.StartGame();
-                        }
-                    }
-                    // 言語選択
-                    // 英語と日本語を切り替え
-                    else if (titleModel.CurrentState == TitleState.Language)
-                    {
-                        if (horizontal >= 0.2)
-                        {
-                            titleModel.SetLanguage(Language.English);
-                        }
-                        else if (horizontal <= -0.2)
-                        {
-                            titleModel.SetLanguage(Language.Japanese);
-                        }
-                        // 息をした時間を加算
-                        if (breath > 0)
-                        {
-                            _breathContinuousTime += Time.deltaTime;
-                        }
-                        else
-                        {
-                            _breathContinuousTime = 0;
-                        }
-                        selectLanguageViewMono.SetBreath(_breathContinuousTime);
-                        // 1秒連続で吹いたら
-                        if (_breathContinuousTime > 1)
-                        {
-                            titleModel.DecideLanguage();
-                        }
+                        case TitleState.Start:
+                            // Clapされればゲーム開始
+                            if (isClap) titleModel.StartGame();
+                            
+                            break;
+
+                        case TitleState.Language:
+                            if (horizontal >= 0.2)
+                            {
+                                titleModel.SetLanguage(Language.English);
+                            }
+                            else if (horizontal <= -0.2)
+                            {
+                                titleModel.SetLanguage(Language.Japanese);
+                            }
+                            // 息をした時間を加算
+                            if (breath > 0)
+                            {
+                                _breathContinuousTime += Time.deltaTime;
+                            }
+                            else
+                            {
+                                _breathContinuousTime = 0;
+                            }
+                            selectLanguageViewMono.SetBreath(_breathContinuousTime);
+                            // 1秒連続で吹いたら
+                            if (_breathContinuousTime > 1)
+                            {
+                                titleModel.DecideLanguage();
+                            }
+                            break;
+
+                        case TitleState.Prologue:
+                            // breathの入力
+                            if (!_isStartBreath && breath > 0)
+                            {
+                                _isStartBreath = true;
+                            }
+                            else if (_isStartBreath && breath == 0)
+                            {
+                                _skipInputCommand.Add(Command.Breath);
+                                _isStartBreath = false;
+                            }
+
+                            // clapの入力
+                            if (isClap) _skipInputCommand.Add(Command.Clap);
+
+                            // パターンマッチング
+                            if (_skipInputCommand.SequenceEqual(_skipCorrectCommand.Take(_skipInputCommand.Count)))
+                            {
+                                // 全て一致すればスキップ
+                                if (_skipInputCommand.Count == _skipCorrectCommand.Count)
+                                    titleViewMono.ProloguePlayableDirector.Stop();
+                            }
+                            else
+                            {
+                                _skipInputCommand.Clear();
+                            }
+                            
+                            break;
 
                     }
                 })
