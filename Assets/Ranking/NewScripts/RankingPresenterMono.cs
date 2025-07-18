ff3b0ffdfd266b461f5bd222a3ffb70b9c561f61
aya097 <akase097@gmail.com>
2025-07-19 03:04:13 +0900

    fix LINQ bug

--- Assets/Ranking/NewScripts/RankingPresenterMono.cs
+++ Assets/Ranking/NewScripts/RankingPresenterMono.cs
@@ -26,6 +26,8 @@ namespace Ranking
             Observable.EveryValueChanged(_rankingModel, r => r.CurrentResultType)
                 .Subscribe(value =>
                 {
+                    var texts = GetRanking(value);
+                    Debug.Log($"RankingPresenterMono: {value} - {string.Join(", ", texts)}");
                     rankingView.SetTexts(GetRanking(value));
                 })
                 .AddTo(this);
@@ -36,50 +38,50 @@ namespace Ranking
             if (resultEnum == ResultEnum.Dirt)
             {
                 // Dirt 小さい順
-                var str = _rankingModel.ResultDataModels.OrderBy(r => r.FinalDirt).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderBy(r => r.FinalDirt).Take(5).Select(r => r.FinalDirt.ToString());
                 return str;
             }
             else if (resultEnum == ResultEnum.Score)
             {
                 // Score 大きい順
-                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalScore).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalScore).Take(5).Select(r => r.FinalScore.ToString());
                 return str;
 
             }
             else if (resultEnum == ResultEnum.Combo)
             {
                 // Combo 大きい順
-                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalCombo).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalCombo).Take(5).Select(r => r.FinalCombo.ToString());
                 return str;
             }
             else if (resultEnum == ResultEnum.Clap)
             {
                 // ClapCount 大きい順
-                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalClapCount).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalClapCount).Take(5).Select(r => r.FinalClapCount.ToString());
                 return str;
             }
             else if (resultEnum == ResultEnum.SumDirt)
             {
                 // SumDirt 小さい順
-                var str = _rankingModel.ResultDataModels.OrderBy(r => r.DirtValueCountSum).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderBy(r => r.DirtValueCountSum).Take(5).Select(r => r.DirtValueCountSum.ToString());
                 return str;
             }
             else if (resultEnum == ResultEnum.Breath)
             {
                 // BreathTime 大きい順
-                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalBreathTime).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.FinalBreathTime).Take(5).Select(r => r.FinalBreathTime.ToString());
                 return str;
             }
             else if (resultEnum == ResultEnum.Calorie)
             {
                 // Calorie 大きい順
-                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.Calorie).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderByDescending(r => r.Calorie).Take(5).Select(r => r.Calorie.ToString());
                 return str;
             }
             else if (resultEnum == ResultEnum.Boss)
             {
                 // BossBattleTime 小さい順
-                var str = _rankingModel.ResultDataModels.OrderBy(r => r.BossBattleTime).Take(5).Select(v => v.ToString());
+                var str = _rankingModel.ResultDataModels.OrderBy(r => r.BossBattleTime).Take(5).Select(r => r.BossBattleTime.ToString());
                 return str;
             }
 
